bafe680b09fc265a51449a9864d2eef1d244fe07
Gil Barbara <gilbarbara@gmail.com>
2017-02-11 21:54:18 -0200

    rewrite remaining gulp tasks in config/tools

--- config/tools.js
+++ config/tools.js
@@ -1,9 +1,16 @@
-/*eslint-disable no-var, vars-on-top, no-console */
+/*eslint-disable no-console */
+const fs = require('fs');
 const path = require('path');
 const exec = require('child_process').exec;
 const del = require('del');
 const chalk = require('chalk');
 const Rsync = require('rsync');
+const ghPages = require('gh-pages');
+const fetch = require('isomorphic-fetch');
+const handlebars = require('handlebars');
+const cpy = require('cpy');
+const _orderBy = require('lodash/orderBy');
+const _take = require('lodash/take');
 
 const args = process.argv.slice(2);
 
@@ -11,18 +18,17 @@ if (!args[0]) {
   console.log(`Valid arguments:
   • publish (push to github)
   • deploy (build & publish)
+  • readme (rebuild README)
   • docs (rebuild documentation)
   • update (if package.json has changed run \`npm update\`)
   • commits (has new remote commits)`
   );
 }
 
-
 function publish() {
   console.log(chalk.blue('Publishing...'));
 
   const destination = 'svgporn@svgporn.com:/home/svgporn/public_html';
-
   const rsync = Rsync.build({
     exclude: ['.DS_Store'],
     progress: true,
@@ -42,8 +48,83 @@ function publish() {
   });
 }
 
+function buildREADME() {
+  console.log(chalk.blue('Generating README...'));
+
+  return new Promise((resolve, reject) => {
+    fetch('https://logos-c87b5.firebaseio.com/items.json', { method: 'get' })
+      .then((response) => response.json())
+      .then((json) => {
+        const logos = _orderBy(json, ['updated', 'name'], ['desc', 'asc']);
+
+        return _take(logos.filter(d => d.public), 50);
+      })
+      .then(items => {
+        const source = fs.readFileSync(path.join(__dirname, 'README.hbs'));
+        const template = handlebars.compile(source.toString());
+        const output = template(items);
+
+        fs.writeFileSync(path.join(__dirname, '..', 'README.md'), output);
+        resolve();
+      })
+      .catch(err => reject(err));
+  });
+}
+
+function getCommit() {
+  console.log(chalk.blue('Getting the last commit...'));
+
+  return new Promise((resolve, reject) => {
+    exec('git log -1 --pretty=%s && git log -1 --pretty=%b', (err, stdout) => {
+      if (err) {
+        return reject(err);
+      }
+
+      const parts = stdout.replace('\n\n', '').split('\n');
+
+      return resolve(`${parts[0]}${(parts[1] ? ` – ${parts[1]}` : '')}`);
+    });
+  });
+}
+
 if (args[0] === 'publish') {
   publish();
+}
+
+if (args[0] === 'master') {
+  console.log(chalk.bgMagenta('Push to master...'));
+
+  buildREADME()
+    .then(getCommit)
+    .then(commit => {
+      console.log(chalk.blue('Copying the files...'));
+
+      cpy(
+        [
+          'logos/**/*.svg',
+          'README.md',
+          'LICENSE.txt',
+        ],
+        path.join(__dirname, '..', '.master'),
+        {
+          cwd: path.join(__dirname, '..'),
+          parents: true,
+        })
+        .then(() => {
+          console.log(chalk.blue('Publishing to github...'));
+          ghPages.publish(path.join(__dirname, '..', '.master'), {
+            branch: 'master',
+            message: commit,
+          }, error => {
+            if (error) {
+              console.log(chalk.red('Something went wrong...', error));
+              return;
+            }
+
+            console.log(chalk.green('Published'));
+          });
+        });
+    });
 }
 
 if (args[0] === 'deploy') {
@@ -59,6 +140,10 @@ if (args[0] === 'deploy') {
 
     publish();
   });
+}
+
+if (args[0] === 'readme') {
+  buildREADME();
 }
 
 if (args[0] === 'docs') {
